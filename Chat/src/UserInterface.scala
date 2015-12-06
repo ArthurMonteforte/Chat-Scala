a60b2bb0a214174f14bb6035715b8b129658f512
ArthurMonteforte <arthur.monteforte@hotmail.com>
2015-12-06 02:59:17 -0200

    Adicionando opção de salvar a conversa

--- Chat/src/UserInterface.scala
+++ Chat/src/UserInterface.scala
@@ -3,13 +3,17 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintStream
 import java.net.Socket
-
 import scala.swing.BorderPanel
 import scala.swing.MainFrame
 import scala.swing.ScrollPane
 import scala.swing.TextArea
 import scala.swing.TextField
 import scala.swing.event.EditDone
+import scala.swing.Button
+import javax.swing.Action
+import scala.swing.event.ButtonClicked
+import javax.swing.text.DefaultCaret
+import scala.swing.FlowPanel
 
 
  /* @author Artur
@@ -17,6 +21,7 @@ import scala.swing.event.EditDone
 object UserInterface {
   
   def main(args: Array[String]): Unit = {
+    var dialogue = ""
     
     val address = "localhost"
     val port = 10001  
@@ -24,6 +29,12 @@ object UserInterface {
     val is = new BufferedReader(new InputStreamReader(sock.getInputStream()))
     val os = new PrintStream(sock.getOutputStream())
     val textArea = new TextArea
+    val buttonSave = new Button("Salvar") {
+      listenTo(this)
+      reactions += {
+        case ButtonClicked(b) => WriteToFile.wirteToFile(dialogue)
+      }
+    }
     val textField = new TextField {
       listenTo(this)
       reactions += {
@@ -38,8 +49,9 @@ object UserInterface {
     val frame = new MainFrame{
       title = "Chat Scala"
       contents = new BorderPanel {
-        layout += new ScrollPane(textArea) -> BorderPanel.Position.North
+        layout += new FlowPanel(textArea) -> BorderPanel.Position.North
         layout += textField -> BorderPanel.Position.South
+        layout += buttonSave -> BorderPanel.Position.East
       }
       size = new Dimension(400, 600)
       centerOnScreen
@@ -50,6 +62,7 @@ object UserInterface {
       while(flag){
         if(is.ready){
           val output = is.readLine
+          dialogue += output + " @@@ "
           textArea.append(output + "\n")
         }
         Thread.sleep(200)
